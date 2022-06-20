a2591a1785df64e78b4f8f2c28c8c965d002f348
Njeta Melvin Ndi <njetami@mail.uc.edu>
2022-06-20 06:37:18 -0400

    update

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,14 +1,22 @@
+import javax.swing.*;
+
 public class BirthMonth
 {
     public static void main(String[] args) {
-       int birthMonth =20;
+       int birthMonth = 5;
 
-       if(birthMonth<1);
-       if(birthMonth>12);
+               if (birthMonth >12)
+
+
         {
             System.out.println("You entered an incorrect Month value " + birthMonth);
         }
-
-
+                else if(birthMonth <1)
+                {
+                    System.out.println("You entered an incorrect Month value " + birthMonth);
+                }
+                else{
+                   System.out.println("You entered a correct Month value" + birthMonth);
+               }
     }
 }
